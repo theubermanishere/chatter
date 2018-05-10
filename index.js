53c0d5ba6b1b48b700e6256a9e836bfeab52154a
Vinayak <vinayakkaushikk@gmail.com>
2018-05-10 15:04:54 +0530

    More mods

--- index.js
+++ index.js
@@ -16,8 +16,18 @@ var io = require('socket.io').listen(server)
 // App setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 // Routes
 app.get('/', home);
 
+// Socket.io
+io.sockets.on('connection', function (socket) {
+    console.log('A new user connected!');
+    socket.emit('info', { msg: 'The world is round, there is no up or down.' });
+    socket.on('msg', (data) => {
+    	socket.emit('msg', data);
+    	console.log(data)
+    	io.sockets.emit('msg', data);
+    });
+});
